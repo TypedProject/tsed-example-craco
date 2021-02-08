848b8811c221e0b0a74e2bbf87a0640d8900a483
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-02-08 21:27:40 +0100

    initial commit

---
+++ packages/app/src/App.js
@@ -0,0 +1,16 @@
+import { Button } from '@project/shared'
+import React from 'react'
+
+function App () {
+  return (
+    <div className="App">
+      <main className="container content__default">
+        <div>
+          <Button>Hello world</Button>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
